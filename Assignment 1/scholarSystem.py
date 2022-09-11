7c4c936d548b48c129fe7ba9d706f743f0badd65
Ash Omaraie <aomarai@ilstu.edu>
2022-09-11 13:13:02 -0500

    Starting assignment 1

---
+++ Assignment 1/scholarSystem.py
@@ -0,0 +1,9 @@
+def createDicts():
+
+
+def main():
+    print("\t\tWelcome to the NC Scholar System\nWe currently include scholars from the following institutes:\n")
+    open()
+
+if __name__ == "__main__":
+    main()
