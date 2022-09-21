e9af0a0737d04b9b030f80583a14bc5eba2cce61
Ash Omaraie <aomarai@ilstu.edu>
2022-09-20 20:23:55 -0500

    Got the romania map working with backtracking with John

--- Assignment 2/Romania_Holiday.py
+++ Assignment 2/Romania_Holiday.py
@@ -3,89 +3,87 @@
 Created on Mon Jan 09 13:23:41 2017
 
 @author: xfang13
-@editors: Ashkan Omaraie, John Skluzacek on September 19, 2022
 
 The Romania problem
 """
 import random
 
-Romania_Map = {'Oradea':[('Zerind',71),('Sibiu',151)],
-               'Zerind':[('Arad',75),('Oradea',71)],
-               'Arad':[('Zerind',75),('Sibiu',140),('Timisoara',118)],
-               'Timisoara':[('Arad',118),('Lugoj',111)],
-               'Lugoj':[('Timisoara',111),('Mehadia',75)],
-               'Mehadia':[('Lugoj',70),('Drobeta',75)],
-               'Drobeta':[('Mehadia',75),('Craiova',120)],
-               'Craiova':[('Drobeta',120),('Rimnicu Vilcea',146),('Pitesti',138)],
-               'Rimnicu Vilcea':[('Craiova',146),('Sibiu',80),('Pitesti',97)],
-               'Sibiu':[('Oradea',151),('Arad',140),('Fagaras',99),('Rimnicu Vilcea',80)],
-               'Fagaras':[('Sibiu',99),('Bucharest',211)],
-               'Pitesti':[('Rimnicu Vilcea',97),('Craiova',138),('Bucharest',101)],
-               'Bucharest':[('Fagaras',211),('Pitesti',101),('Giurgiu',90),('Urziceni',85)],
-               'Giurgiu':[('Bucharest',90)],
-               'Urziceni':[('Bucharest',85),('Vaslui',142),('Hirsova',98)],
-               'Neamt':[('Iasi',87)],
-               'Iasi':[('Neamt',87),('Vaslui',92)],
-               'Vaslui':[('Iasi',92),('Urziceni',142)],
-               'Hirsova':[('Urziceni',98),('Eforie',86)],
-               'Eforie':[('Hirsova',86)]           
-              }
+Romania_Map = {'Oradea': [('Zerind', 71), ('Sibiu', 151)],
+               'Zerind': [('Arad', 75), ('Oradea', 71)],
+               'Arad': [('Zerind', 75), ('Sibiu', 140), ('Timisoara', 118)],
+               'Timisoara': [('Arad', 118), ('Lugoj', 111)],
+               'Lugoj': [('Timisoara', 111), ('Mehadia', 75)],
+               'Mehadia': [('Lugoj', 70), ('Drobeta', 75)],
+               'Drobeta': [('Mehadia', 75), ('Craiova', 120)],
+               'Craiova': [('Drobeta', 120), ('Rimnicu Vilcea', 146), ('Pitesti', 138)],
+               'Rimnicu Vilcea': [('Craiova', 146), ('Sibiu', 80), ('Pitesti', 97)],
+               'Sibiu': [('Oradea', 151), ('Arad', 140), ('Fagaras', 99), ('Rimnicu Vilcea', 80)],
+               'Fagaras': [('Sibiu', 99), ('Bucharest', 211)],
+               'Pitesti': [('Rimnicu Vilcea', 97), ('Craiova', 138), ('Bucharest', 101)],
+               'Bucharest': [('Fagaras', 211), ('Pitesti', 101), ('Giurgiu', 90), ('Urziceni', 85)],
+               'Giurgiu': [('Bucharest', 90)],
+               'Urziceni': [('Bucharest', 85), ('Vaslui', 142), ('Hirsova', 98)],
+               'Neamt': [('Iasi', 87)],
+               'Iasi': [('Neamt', 87), ('Vaslui', 92)],
+               'Vaslui': [('Iasi', 92), ('Urziceni', 142)],
+               'Hirsova': [('Urziceni', 98), ('Eforie', 86)],
+               'Eforie': [('Hirsova', 86)]
+               }
+
 
 class PathFindingAgent(object):
     def __init__(self, Map):
         self.map = Map
         self.route = []
         self.total_cost = 0
+        self.current_city = None
+        self.visited = []
+
     def solve(self, start, end, budget):
         flag = True
         self.route.append(start)
+        self.visited.append(start)
         self.current_city = start
-        cities = [start]
+
         while flag:
+            print(self.route)
+            self.visited.append(self.current_city)
+            # basically backtracking, if it gets caught in a dead end it resets itself to the starting city
+            if self.route.count(self.current_city) == 2:
+                del self.route[-1]
+                self.current_city = self.route[0]
+                self.route.clear()
+                self.route.append(self.current_city)
+                self.total_cost = 0
+
             if self.total_cost >= budget:
                 print('Gone over budget. Please increase the budget.')
                 quit(0)
             elif self.current_city == end:
                 flag = False
             else:
-                #Make a list of next cities: Observing
+                # Make a list of next cities: Observing
                 cities = [city for city, cost in self.map[self.current_city]]
-                #Make a list of next costs: Observing
+                # Make a list of next costs: Observing
                 costs = [cost for city, cost in self.map[self.current_city]]
-                print("Next cities for: ", self.current_city, cities)
-                #print(costs)
-                #See if the destination is in the next cities' list
+                # See if the destination is in the next cities' list
                 try:
-                    for city in cities:
-                        if city in self.route:
-                            cities.remove(city)
-                            costs.remove(costs.index(city))
                     i = cities.index(end)
                     self.current_city = end
                     self.route.append(self.current_city)
                     self.total_cost += costs[i]
-                #if not then random walk
+                # if not then random walk
                 except ValueError:
-                    next_city, cost = random.sample(self.map[self.current_city],1)[0]
-                    temp_flag = True
-                    while temp_flag:
-                        if next_city in cities:
-                            temp_flag = False
-                        else:
-                            next_city, cost = random.sample(self.map[self.current_city], 1)[0]
-                            if len(cities) == 0:
-                                print("Dead end reached. No cities available to travel to.")
-                                temp_flag = False
-                                quit(0)
-                    #Add the current cost to the total
+                    next_city, cost = random.sample(self.map[self.current_city], 1)[0]
+                    # Add the current cost to the total
                     self.total_cost += cost
-                    #Set next city to the current city
+                    # Set next city to the current city
                     self.current_city = next_city
-                    #Add the current city to the route
+                    # Add the current city to the route
                     self.route.append(self.current_city)
-                
-                
+
+
 if __name__ == '__main__':
     agent = PathFindingAgent(Romania_Map)
-    agent.solve('Bucharest','Arad', 10000)
-    print (agent.route, agent.total_cost)                
+    agent.solve('Bucharest', 'Arad', 5000)
+    print(agent.route, agent.total_cost)
