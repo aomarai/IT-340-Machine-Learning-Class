afc24dccfa7fdcc012f913fbda8e1b9b14de2579
Ash <ashomaraie@icloud.com>
2022-10-19 19:45:52 -0500

    Finished policy evaluation.py

---
+++ Assignment 3/policy evaluation.py
@@ -0,0 +1,60 @@
+# Made by John Skluzacek and Ashkan Omaraie 10/19/2022
+
+# What should be chosen depending on policy
+policy_map = {
+    1: {
+        'A': {1: 1, 2: 0},
+        'B': {1: 1, 2: 0},
+        'C': {1: 1, 2: 0}},
+    2: {
+        'A': {1: 0, 2: 1},
+        'B': {1: 0, 2: 1},
+        'C': {1: 0, 2: 1}},
+    3: {
+        'A': {1: .4, 2: .6},
+        'B': {1: 1, 2: 0},
+        'C': {1: 0, 2: 1}}
+}
+action_set = {1, 2}
+state_transition_model_2 = {
+    'A': {1: {'B': .9, 'C': .1}, 2: {'B': .1, 'C': .9}},
+    'B': {1: {'D': .9, 'A': .1}, 2: {'D': .1, 'A': .9}},
+    'C': {1: {'A': .9, 'D': .1}, 2: {'A': .1, 'D': .9}}
+}
+# The value of each node
+state_value_table = {'A': 0, 'B': 0, 'C': 0, 'D': 100}
+# Gamma
+discounting_factor = .8
+reward_model = -10
+
+
+def policy_eval(iterations, policy):
+    for i in range(iterations):
+        vals = []
+        # Check each state
+        for state in state_transition_model_2:
+            if not state == 'D':
+                state_val = 0
+                # Check each action
+                for action in action_set:
+                    # Check each subsequent action
+                    for sub_state in state_transition_model_2[state][action]:
+                        # One or zero * .9 or .1
+                        state_val += state_transition_model_2[state][action][sub_state] * policy_map[policy][state][
+                            action] * (reward_model + discounting_factor * state_value_table[sub_state])
+                        vals.append(state_val)
+                        state_value_table[state] = round(state_val, 2)
+    # formatting
+
+    print('Vπ(A)= ', state_value_table['A'])
+    print('Vπ(B)= ', state_value_table['B'])
+    print('Vπ(C)= ', state_value_table['C'])
+    print('Vπ(D)= ', state_value_table['D'])
+
+if __name__ == '__main__':
+    print('Policy 1')
+    policy_eval(100, 1)
+    print('\nPolicy 2')
+    policy_eval(100, 2)
+    print('\nPolicy 3')
+    policy_eval(100, 3)
